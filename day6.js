4da8dd2cf49cf93dd49c5c4d9cf0270e52067728
Agrawal-Vansh <vanshaggrawal1@gmail.com>
2024-07-30 22:35:00 +0530

    Day6.js added

---
+++ day6.js
@@ -0,0 +1,69 @@
+// **Tasks/Activities:**
+
+// **Activity 1: Array Creation and Access**
+// - **Task 1:** Create an array of numbers from 1 to 5 and log the array to the console.
+let arr=[1,2,3,4,5]
+console.log(arr); // [ 1, 2, 3, 4, 5 ]
+
+// - **Task 2:** Access the first and last elements of the array and log them to the console.
+console.log(arr[0]); // 1
+console.log(arr[arr.length-1]); //5 
+
+// **Activity 2: Array Methods (Basic)**
+
+// - **Task 3:** Use the `push` method to add a new number to the end of the array and log the updated array.
+arr.push(6);
+console.log(arr); // [ 1, 2, 3, 4, 5, 6 ]
+
+// - **Task 4:** Use the `pop` method to remove the last element from the array and log the updated array.
+arr.pop()
+console.log(arr);// [ 1, 2, 3, 4, 5 ]
+
+
+// - **Task 5:** Use the `shift` method to remove the first element from the array and log the updated array.
+arr.shift()
+console.log(arr);  // [ 2, 3, 4, 5 ]
+
+
+// - **Task 6:** Use the `unshift` method to add a new number to the beginning of the array and log the updated array.
+arr.unshift(1)
+console.log(arr); // [ 1, 2, 3, 4, 5 ]
+
+
+// **Activity 3: Array Methods (Intermediate)**
+// - **Task 7:** Use the `map` method to create a new array where each number is doubled and log the new array.
+let arr_double=arr.map((i)=>i*2)
+console.log(arr_double);
+
+
+// - **Task 8:** Use the `filter` method to create a new array with only even numbers and log the new array.
+let arr_even=arr.filter((i)=>i%2==0)
+console.log(arr_even);
+
+// - **Task 9:** Use the `reduce` method to calculate the sum of all numbers in the array and log the result.
+const sum = arr.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  
+  console.log(sum); // Output: 15
+
+// **Activity 4: Array Iteration**
+// - **Task 10:** Use a `for` loop to iterate over the array and log each element to the console.
+for(let i=0;i<arr.length;i++)
+    console.log(arr[i]);
+
+// - **Task 11:** Use the `forEach` method to iterate over the array and log each element to the console.
+arr.forEach((element, index) => {
+    console.log(element);
+  });
+
+
+// **Activity 5: Multi-dimensional Arrays**
+// - **Task 12:** Create a two-dimensional array (matrix) and log the entire array to the console.
+let mat=[[1,2,3],[4,5,6]];
+console.log(mat); // [ [ 1, 2, 3 ], [ 4, 5, 6 ] ]
+
+// - **Task 13:** Access and log a specific element from the two-dimensional array.
+let num1=mat[0][1];
+console.log(num1);// output 2 
+
